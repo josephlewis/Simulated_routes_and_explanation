41bf27107dbdea75f99365673b1bdeae1e267990
josephlewis <josephlewis1992@gmail.com>
2022-09-27 10:45:48 +0100

    added data, code, and outputs

---
+++ R/Functions.R
@@ -0,0 +1,273 @@
+normalise_raster <- function(x){(x-terra::minmax(dem)[1])/(terra::minmax(dem)[2]-terra::minmax(dem)[1])}
+
+compare_lcps <- function(cs_list, cfs, cf1_index, cf2_index, pts) {
+  
+  slope_cs1 <- cs_list[[cf1_index]]
+  slope_cs2 <- cs_list[[cf2_index]]
+  
+  lcp1 <- leastcostpath::create_lcp(x = slope_cs1, origin = pts[1], destination = pts[2])
+  
+  lcp2 <- leastcostpath::create_lcp(x = slope_cs2, origin = pts[1], destination = pts[2])
+  
+  pdi <- leastcostpath::PDI_validation(lcp = lcp1, comparison = lcp2)
+  
+  lcp1$pts_dist <- pdi$max_distance
+  lcp1$pdi <- pdi$pdi
+  lcp1$normalised_pdi <- pdi$normalised_pdi
+  lcp1$cf1 <- cfs[cf1_index]
+  lcp1$cf2 <- cfs[cf2_index]
+  lcp1$cf1_type <- NA
+  lcp1$cf2_type <- NA
+
+  time_cfs <- c("tobler", "tobler offpath", "rees", "davey", "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", "modified tobler", "garmy", "Kondo-Saino", "naismith", "campbell", "campbell 2019")
+  
+  energy_cfs <- c("herzog", "llobera-sluckin", "minetti")
+  
+  if(lcp1$cf1 %in% time_cfs) {
+    lcp1$cf1_type <- "Time-based"
+  } else if (lcp1$cf1 %in% energy_cfs) {
+    lcp1$cf1_type <- "Energy-based"
+  } else {
+    lcp1$cf1_type <- "Wheel-based"
+  }
+  
+  if(lcp1$cf2 %in% time_cfs) {
+    lcp1$cf2_type <- "Time-based"
+  } else if (lcp1$cf2 %in% energy_cfs) {
+    lcp1$cf2_type <- "Energy-based"
+  } else {
+    lcp1$cf2_type <- "Wheel-based"
+  }
+  
+  return(lcp1)
+  
+}
+
+cost <- function(cost_function, crit_slope, percentile) {
+  
+  cfs <- c("tobler", "tobler offpath", "davey", 'rees', "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", "modified tobler", 'garmy', 'kondo-saino', "wheeled transport", "herzog", "llobera-sluckin", 'naismith', 'minetti', 'campbell', "campbell 2019", "sullivan")
+  
+  if (inherits(cost_function, "character")) {
+    if (!cost_function %in% cfs) {
+      stop("cost_function argument is invalid. See details for accepted cost functions")
+    }
+    
+    # mathematical slope to degrees
+    slope2deg <- function(slope) { (atan(slope) * 180/pi)}
+    
+    # degrees to radians
+    deg2rad <- function(deg) {(deg * pi) / (180)}
+    
+    # Tobler Hiking Function measured in km/h. Divide by 3.6 to turn into m/s
+    if (cost_function == "tobler") {
+      
+      # 3.6 converts from km/h to m/s
+      cf <- function(x) {
+        (6 * exp(-3.5 * abs(x + 0.05))) / 3.6
+      }
+    }
+    
+    if (cost_function == "tobler offpath") {
+      
+      cf <- function(x) {
+        ((6 * exp(-3.5 * abs(x + 0.05))) * 0.6) / 3.6
+      }
+      
+    }
+    
+    # requires mathematical slope to be in radians. Necessary to convert mathematical slope to degrees and then to radians
+    if (cost_function == "davey") { 
+      
+      cf <- function(x, y = 1.40, z = 2.8) { 
+        x_deg <- slope2deg(x)
+        x_rad <- deg2rad(x_deg)
+        
+        (y * exp(-z*abs(x_rad)))
+      }
+      
+    }
+    
+    if (cost_function == "rees") { 
+      
+      cf <- function(x) { 
+        (1 / (0.75 + 0.09 * abs(x) + 14.6 * (abs(x))^2))
+      }
+      
+    }
+    
+    if (cost_function == "irmischer-clarke male") {
+      
+      cf <- function(x) {
+        (0.11 + exp(-(abs(x) * 100 + 5)^2/(2 * 30^2)))
+        
+      }
+      
+    }
+    
+    if (cost_function == "irmischer-clarke offpath male") {
+      
+      cf <- function(x) {
+        (0.11 + 0.67 * exp(-(abs(x) * 100 + 2)^2/(2 * 30^2)))
+      }
+      
+    }
+    
+    if (cost_function == "irmischer-clarke female") {
+      
+      cf <- function(x) {
+        (0.95 * (0.11 + exp(-(abs(x) * 100 + 5)^2/(2 * 30^2))))
+      }
+      
+    }
+    
+    if (cost_function == "irmischer-clarke offpath female") {
+      
+      cf <- function(x) {
+        0.95 * (0.11 + 0.67 * exp(-(abs(x) * 100 + 2)^2/(2 * 30^2)))
+      }
+      
+    }
+    
+    if (cost_function == "modified tobler") {
+      
+      cf <- function(x) {
+        (4.8 * exp(-5.3 * abs((x * 0.7) + 0.03)))
+      }
+      
+    }
+    
+    if (cost_function == "garmy") {
+      
+      cf <- function(x) { 
+        (4 * exp(-0.008 * (slope2deg(x)^2)))
+      }
+      
+    }
+    
+    if (cost_function == "kondo-saino") {
+      
+      cf <- function(x) { 
+        ifelse(abs(x) >= -0.07, (5.1 * exp(-2.25 * abs(x + 0.07))), (5.1 * exp(-1.5 * abs(x + 0.07))))
+      }
+      
+    }
+    
+    if (cost_function == "wheeled transport") {
+      
+      cf <- function(x) {
+        (1/(1 + ((abs(x) * 100)/crit_slope)^2))
+      }
+      
+    }
+    
+    if (cost_function == "herzog") {
+      
+      cf <- function(x) {
+        
+        (1/((1337.8 * abs(x)^6) + (278.19 * abs(x)^5) - (517.39 * abs(x)^4) - (78.199 * abs(x)^3) + (93.419 * abs(x)^2) + (19.825 * abs(x)) + 1.64))
+        
+      }
+      
+    }
+    
+    if (cost_function == "llobera-sluckin") {
+      
+      cf <- function(x) {
+        (1/(2.635 + (17.37 * abs(x)) + (42.37 * abs(x)^2) - (21.43 * abs(x)^3) + (14.93 * abs(x)^4)))
+      }
+      
+    }
+    
+    if (cost_function == "naismith") {
+      
+      cf <- function(x) {
+        
+        x_deg <- slope2deg(x)
+        x_rad <- deg2rad(x_deg)
+        
+        ifelse(x_deg > 0, (1 / (0.72 + 6 * tan(x_rad))), ifelse(x_deg <= -12, (1 / (0.72 - 2 * tan(x_rad))), ifelse(x_deg > -12 & x_deg <= -5, (1 / (0.72 + 2 * tan(x_rad))), 1.40)))
+      }
+    }
+    
+    if (cost_function == "minetti") { 
+      
+      cf <- function(x) { 
+        1/ ((280.5 * abs(x)^5) - (58.7 * abs(x)^4) - (76.8 * abs(x)^3) + (51.9 * abs(x)^2) + (19.6 * abs(x)) + 2.5)
+      } 
+      
+    }
+    
+    if (cost_function == "campbell") { 
+      
+      cf <- function(x) { 
+        x_deg <- slope2deg(x)
+        
+        1.662 - (5.191 * 10^-3) * x_deg - (1.127*10^-3) * x_deg^2
+      }
+      
+    }
+    
+    if (cost_function == "campbell 2019") {
+      
+      percentile_choice <- c(0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 
+                             0.95, 0.99)
+      
+      if (!percentile %in% percentile_choice) {
+        stop("percentile argument is invalid. Expecting percentile value of 0.01, 0.05, 0.10, 0.15, 0.20,
+        0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95 or 0.99")
+      }
+      
+      term_a <- c(-2.1, -1.527, -1.568, -1.626, -1.71, -1.822, -1.858, -1.891, -1.958, -2.05, -2.171, -2.317, -2.459, -2.647, -2.823, 
+                  -3.067, -3.371, -3.661, -3.06, -3.485, -4)
+      term_b <- c(12.273, 14.041, 13.328, 11.847, 10.154, 8.827, 8.412, 8.584, 8.96, 9.402, 10.064, 10.712, 11.311, 12.089, 12.784, 
+                  13.888, 15.395, 17.137, 16.653, 17.033, 13.903)
+      term_c = c(21.816, 36.813, 38.892, 38.231, 36.905, 37.111, 39.995, 44.852, 50.34, 56.172, 63.66, 71.572, 79.287, 89.143, 98.697, 
+                 113.655, 134.409, 159.027, 138.875, 138.04, 123.515)
+      term_d = c(0.263, 0.32, 0.404, 0.481, 0.557, 0.616, 0.645, 0.649, 0.649, 0.646, 0.628, 0.608, 0.599, 0.576, 0.566, 0.518, 0.443, 
+                 0.385, 0.823, 1.179, 1.961)
+      term_e = c(-0.00193, -0.00273, -0.00323, -0.00356, -0.00389, -0.00402, -0.0043, -0.00443, -0.00457, -0.0046, -0.00463, -0.00451, 
+                 -0.00461, -0.00465, -0.00493, -0.00488, -0.00472, -0.00534, -0.01386, -0.01252, -0.01081)
+      
+      lorentz_function_terms <- data.frame(percentile = percentile_choice, term_a, term_b, term_c, term_d, term_e)
+      terms <- lorentz_function_terms[lorentz_function_terms$percentile == percentile, ]
+      
+      cf <- function(x){
+        (terms$term_c*(1/(pi*terms$term_b*(1+((slope2deg(x)-terms$term_a)/terms$term_b)^2)))+terms$term_d)+terms$term_e*slope2deg(x)
+      }
+    }
+    
+    if (cost_function == "sullivan") {
+      
+      percentile_choice <- c(0.167, 0.5, 0.833)
+      
+      if (!percentile %in% percentile_choice) {
+        stop("percentile argument is invalid. Expecting percentile value of 0.167, 0.5, 0.833")
+      }
+      
+      term_a <- c(-3.3717, -2.8292, -2.2893)
+      term_b <- c(25.8255, 20.9482, 19.4024)
+      term_c <- c(92.6594, 77.6346, 65.3577)
+      term_d <- c(-0.1624, 0.2228, 0.6226)
+      term_e <- c(0.0019, -0.0004, -0.0020)
+      
+      lorentz_function_terms <- data.frame(percentile = percentile_choice, term_a, term_b, term_c, term_d, term_e)
+      terms <- lorentz_function_terms[lorentz_function_terms$percentile == percentile, ]
+      
+      cf <- function(x){
+        (terms$term_c*(1/(pi*terms$term_b*(1+((slope2deg(x)-terms$term_a)/terms$term_b)^2)))+terms$term_d)+terms$term_e*slope2deg(x)
+      }
+      
+    }
+    
+  }
+  
+  if(is.function(cost_function)) {
+    
+    cf <- cost_function
+    
+  }
+  
+  return(cf)
+}
+
+
---
+++ R/Main.R
@@ -0,0 +1,202 @@
+library(devtools)
+install_github("josephlewis/leastcostpath@dev")
+library(leastcostpath)
+library(terra)
+library(sf)
+library(dplyr)
+library(ggplot2)
+
+#setwd("/export/home/jl2094/routepath_lengths")
+#setwd("~/28_06_2021/Simulated_Routes_and_Equifinality")
+
+################
+################
+#### SET-UP ####
+################
+################
+
+set.seed(1)
+nsims <- 1000
+neigh <- 4
+
+source("./R/Functions.R")
+
+#############################################
+#############################################
+#### PREPROCESSS DIGITAL ELEVATION MODEL ####
+#############################################
+#############################################
+
+dem <- terra::rast("./Data/FS_240.tif")
+dem <- normalise_raster(dem)
+dem <- dem * 10
+
+####################################
+####################################
+#### CREATE COST FUNCTION TABLE ####
+####################################
+####################################
+
+cfs <- c("tobler", "tobler offpath", "modified tobler", "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", 'rees', "davey", 'garmy', 'kondo-saino', 'naismith', 'campbell', "campbell 2019", "herzog", "llobera-sluckin", 'minetti', "wheeled transport")
+
+index_table <- expand.grid(1:length(cfs), 1:length(cfs))
+
+##############################
+##############################
+#### CREATE COST SURFACES ####
+##############################
+##############################
+
+cs_list <- apply(X = data.frame(cfs), MARGIN = 1, FUN = function(x) {  
+  cs <- leastcostpath::create_slope_cs(x = dem, cost_function = x, neighbours = neigh, crit_slope = 12, percentile = 0.5)
+  return(cs)
+  }
+)
+
+###########################################################################
+###########################################################################
+#### CALCULATE AND COMPARE LCPs CREATED USING DIFFERENT COST FUNCTIONS ####
+###########################################################################
+###########################################################################
+
+lcps <- list()
+
+for (i in 1:nsims) {
+  
+  print(i)
+  
+  pts <- sf::st_sample(sf::st_as_sfc(sf::st_bbox(dem)), 2, type = "random")
+  
+  compare_lcp <- apply(X = index_table, MARGIN = 1, FUN = function(x) { compare_lcps(cs_list = cs_list, cfs = cfs, cf1_index = x[1], cf2_index = x[2], pts = pts)})
+  
+  compare_lcp <- do.call(rbind, compare_lcp)
+  compare_lcp$sim_no <- i
+  
+  lcps[[i]] <- compare_lcp  
+
+}
+
+lcps2 <- do.call(rbind, lcps)
+
+lcps3 <- sf::st_drop_geometry(lcps2)
+
+#FS = fractal surface, 240 = 2.40 fractal dimension, 4 = neighbours, 1000 = nsims 
+write.csv(lcps3, file = "./Output/Data/FS_240_4_1000.csv", row.names = FALSE)
+
+############################
+#### METHODS PLOT ONE ####
+############################
+
+cf_df <- list()
+
+for (i in 1:length(cfs)) {
+  
+  print(i)
+  print(cfs[i])
+  
+  time_cfs <- c("tobler", "tobler offpath", "rees", "davey", "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", "modified tobler", "garmy", "kondo-saino", "naismith", "campbell", "campbell 2019")
+  
+  energy_cfs <- c("herzog", "llobera-sluckin", "minetti")
+  
+  if(cfs[i] %in% time_cfs) { 
+    type <- "Time-based"
+  } else if (cfs[i] %in% energy_cfs) { 
+    type <- "Energy-based"  
+  } else { 
+    type <- "Wheel-based"  
+  }
+  
+  cf <- cost(cost_function = cfs[i], crit_slope = 12, percentile = 0.5)
+  
+  slope <- seq(-0.4, 0.4, 0.01)
+  vals <- 1/cf(slope)
+  vals <- (vals - min(vals)) / (max(vals) - min(vals))
+  
+  cf_df[[i]] <- data.frame(slope = slope, cf_vals = vals, cf = cfs[i], type = type)
+  
+}
+
+cf_df <- do.call(rbind, cf_df)
+cf_df$type <- factor(cf_df$type, levels = c("Time-based", "Energy-based", "Wheel-based"))
+cf_df$cf <- factor(cf_df$cf, levels = cfs)
+
+methods_plot1 <- ggplot(cf_df) + 
+  geom_vline(xintercept = 0, lty = 2, colour = "grey80", size = 0.2) +
+  geom_line(aes(x = slope, y = cf_vals, group = cf, colour = type)) +
+  facet_wrap(~cf, nrow = 6) + 
+  scale_colour_manual(values = c("#003f5c", "#bc5090", "#ffa600")) + 
+  labs(x = "Mathematical Slope", y = "Relative cost", colour = "Theory type") + 
+  theme_classic() + 
+  theme(legend.position = "bottom")
+
+ggplot2::ggsave(filename = "methods_plot_01.png", plot = methods_plot1, path = "./Output/Figures/", dpi = 300, width = 6, height = 6)
+
+###################
+###################
+#### ANALYSIS #####
+###################
+###################
+
+############################
+##### RESULTS PLOT ONE #####
+############################
+
+lcps <- read.csv("./Output/Data/FS_240_4_1000.csv")
+lcps2 <- lcps[lcps$normalised_pdi == 0,]
+
+lcp_df1 <- as.data.frame(table(lcps2$cf1_type, lcps2$cf2_type))
+
+lcp_df1 <- lcp_df1 %>%
+  group_by(Var1) %>%
+  mutate(perc = Freq /sum(Freq) * 100, Var2 = Var2) %>%
+  arrange(Var1)
+
+lcp_df1$Var1 <- factor(lcp_df1$Var1, levels = rev(c("Time-based", "Energy-based", "Wheel-based")))
+lcp_df1$Var2 <- factor(lcp_df1$Var2, levels = c("Time-based", "Energy-based", "Wheel-based"))
+
+results_plot1 <- ggplot(lcp_df1, aes(x = Var1, y = perc, fill = Var2)) +
+  geom_bar(position="fill", stat="identity", show.legend = TRUE) +
+  labs(x = "Route Type", y = "Percentage (%)", fill = "Theory Type") +
+  geom_text(aes(label = paste0(round(perc, 2))),
+            position = position_fill(vjust = 0.5), size = 4) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_discrete(expand = expansion(mult = c(0.3, 0))) + 
+  scale_fill_manual(values = c("#003f5c", "#bc5090", "#ffa600")) + 
+  coord_flip() + 
+  theme_classic() + 
+  theme(axis.line.y =element_blank(), axis.ticks.y = element_blank(), legend.position = "bottom") + 
+  theme(legend.justification = "right", legend.margin = margin(t = 2, r = 4, b = 2, l = 2, unit = "mm"))
+
+ggplot2::ggsave(plot = results_plot1, filename = "./Output/Figures/results_plot_01.png", width = 10, height = 4, dpi = 300)
+
+############################
+##### RESULTS PLOT TWO #####
+############################
+
+lcp_df2 <- as.data.frame(table(lcps2$cf1_type, lcps2$cf2))
+
+lcp_df2 <- lcp_df2 %>%
+  group_by(Var1) %>%
+  mutate(perc = Freq /sum(Freq) * 100, Var2 = Var2) %>%
+  filter(Var1 == "Wheel-based") %>%
+  arrange(Freq)
+
+colnames(lcp_df2) <- c("cf1_type", "cf2", "Freq", "perc")
+lcp_df2 <- left_join(x = lcp_df2, y = unique(lcps2[c("cf2", "cf2_type")]), by = "cf2")
+lcp_df2$cf2_type <- factor(lcp_df2$cf2_type, levels = c("Time-based", "Energy-based", "Wheel-based"))
+lcp_df2$Var2 <- factor(lcp_df2$cf2, levels = as.character(unique(lcp_df2$cf2)))
+
+results_plot2 <- ggplot(lcp_df2, aes(x = Var2, y = perc, fill = cf2_type)) +
+  geom_bar(stat="identity", show.legend = TRUE) +
+  labs(x = "Cost Function", y = "Percentage (%)", fill = "Theory Type") +
+  geom_text(aes(label = paste0(round(perc, 2))), hjust = "left", size = 4, nudge_y = 2) + 
+  scale_fill_manual(values = c("#003f5c", "#bc5090", "#ffa600")) + 
+  lims(y = c(0, 100)) + 
+  coord_flip() + 
+  theme_classic() + 
+  theme(legend.position = "bottom") + 
+  theme(legend.title = element_text( size=8), legend.text=element_text(size=8))
+
+results_plot2
+
+ggplot2::ggsave(plot = results_plot2, filename = "./Output/Figures/results_plot_02.png", width = 6, height = 4, dpi = 300)
